c0e2b95c32a01b35e375a56b99991cb5afcea7e9
Nick Ludwig <nick.ludwig223@gmail.com>
2020-11-19 20:46:26 -0600

    updated files

--- sql_server_connection.py
+++ sql_server_connection.py
@@ -6,4 +6,4 @@ DATABASE = 'bank_data' # altered for security
 DRIVER = 'SQL Server Native Client 11.0'  # altered for security
 DATABASE_CONNECTION = f'mssql://@{SERVER}/{DATABASE}?driver={DRIVER}'
 engine = create_engine(DATABASE_CONNECTION)
-connection = engine.connect()
+#connection = engine.connect()

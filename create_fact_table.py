deb63615b29bea5d08df1729b6f7111538098e8f
Sanyam Khurana <sanyam@sanyamkhurana.com>
2020-11-21 15:48:14 -0400

    Fix formatting

--- create_fact_table.py
+++ create_fact_table.py
@@ -1,34 +1,57 @@
-from create_dimensions import dim_branch, dim_bank, dim_area_population, \
-dim_location, dim_assets, dim_date, dim_charter, dim_fdic, dim_currency, dim_deposit_code
-from data_extraction import combined_df
-from sql_server_connection import engine
 import sqlalchemy as sa
 
-dim_list2 = [dim_branch, dim_bank, dim_area_population,
-dim_location, dim_assets, dim_date, dim_charter, dim_fdic, dim_currency, dim_deposit_code]
+from create_dimensions import (
+    dim_area_population,
+    dim_assets,
+    dim_bank,
+    dim_branch,
+    dim_charter,
+    dim_currency,
+    dim_date,
+    dim_deposit_code,
+    dim_fdic,
+    dim_location,
+)
+from data_extraction import combined_df
+from sql_server_connection import engine
 
-branch_cols = [column for column in dim_branch.columns[1:]] # skipping branch_key column
+dim_list2 = [
+    dim_branch,
+    dim_bank,
+    dim_area_population,
+    dim_location,
+    dim_assets,
+    dim_date,
+    dim_charter,
+    dim_fdic,
+    dim_currency,
+    dim_deposit_code,
+]
+
+branch_cols = [
+    column for column in dim_branch.columns[1:]
+]  # skipping branch_key column
 branch_cols
-fact_fdic = combined_df.merge(dim_branch, on=branch_cols) 
-fact_fdic.drop(columns= branch_cols, inplace=True)  
+fact_fdic = combined_df.merge(dim_branch, on=branch_cols)
+fact_fdic.drop(columns=branch_cols, inplace=True)
 
 assets_cols = [column for column in dim_assets.columns[1:]]
 fact_fdic = fact_fdic.merge(dim_assets, on=assets_cols)
 
 bank_cols = [column for column in dim_bank.columns[1:]]
 bank_cols
-fact_fdic = fact_fdic.merge(dim_bank, on= bank_cols)
+fact_fdic = fact_fdic.merge(dim_bank, on=bank_cols)
 fact_fdic.drop(columns=bank_cols, inplace=True)
 
-dim_location_cols = [column for column in dim_location.columns[1:]]   
+dim_location_cols = [column for column in dim_location.columns[1:]]
 fact_fdic = fact_fdic.merge(dim_location, on=dim_location_cols)
 fact_fdic.drop(columns=dim_location_cols, inplace=True)
 
-dim_date_cols = [column for column in dim_date.columns[1:]]   
+dim_date_cols = [column for column in dim_date.columns[1:]]
 fact_fdic = fact_fdic.merge(dim_date, on=dim_date_cols)
 fact_fdic.drop(columns=dim_date_cols, inplace=True)
 
-dim_charter_cols = [column for column in dim_charter.columns[1:]]  
+dim_charter_cols = [column for column in dim_charter.columns[1:]]
 fact_fdic = fact_fdic.merge(dim_charter, on=dim_charter_cols)
 fact_fdic.drop(columns=dim_charter_cols, inplace=True)
 
@@ -36,17 +59,19 @@ dim_fdic_cols = [column for column in dim_fdic.columns[1:]]
 fact_fdic = fact_fdic.merge(dim_fdic, on=dim_fdic_cols)
 fact_fdic.drop(columns=dim_fdic_cols, inplace=True)
 
-dim_currency_cols = [column for column in dim_currency.columns[1:]] 
+dim_currency_cols = [column for column in dim_currency.columns[1:]]
 fact_fdic = fact_fdic.merge(dim_currency, on=dim_currency_cols)
 fact_fdic.drop(columns=dim_currency_cols, inplace=True)
 
-col_list = [col for col in fact_fdic.columns] # renaming the duplicate columns
-col_list[3] = 'delete_this'
-col_list[4] = 'delete_this2'
-fact_fdic.columns = col_list # renaming the columns
-fact_fdic.drop(columns=['delete_this', 'delete_this2'], inplace=True) # drops the duplicate columns
+col_list = [col for col in fact_fdic.columns]  # renaming the duplicate columns
+col_list[3] = "delete_this"
+col_list[4] = "delete_this2"
+fact_fdic.columns = col_list  # renaming the columns
+fact_fdic.drop(
+    columns=["delete_this", "delete_this2"], inplace=True
+)  # drops the duplicate columns
 
-dim_area_population_cols = [column for column in dim_area_population.columns[1:]] 
+dim_area_population_cols = [column for column in dim_area_population.columns[1:]]
 fact_fdic = fact_fdic.merge(dim_area_population, on=dim_area_population_cols)
 fact_fdic.drop(columns=dim_area_population_cols, inplace=True)
 
@@ -55,82 +80,120 @@ fact_fdic = fact_fdic.merge(dim_deposit_code, on=dim_deposit_code_cols)
 fact_fdic.drop(columns=dim_deposit_code_cols, inplace=True)
 
 with engine.connect() as connection:
-    # Creating the fact table in SQL Server and sending the data to SQL Server.  
-    fact_fdic.to_sql('fact_fdic', connection, index=False, if_exists='replace',
-                    dtype={'branch_office_deposits': sa.BigInteger,
-                            'total_assets': sa.BigInteger,
-                            'total_domestic_deposits': sa.BigInteger,
-                            'total_deposits': sa.BigInteger,
-                            'branch_key': sa.INTEGER,
-                            'bank_key': sa.INTEGER,
-                            'location_key': sa.INTEGER,
-                            'date_key': sa.INTEGER,
-                            'charter_key': sa.INTEGER,
-                            'fdic_key': sa.INTEGER,
-                            'currency_key': sa.INTEGER,
-                            'area_population_key': sa.INTEGER,
-                            'deposit_code_key': sa.INTEGER,
-                            'assets_key': sa.INTEGER})
+    # Creating the fact table in SQL Server and sending the data to SQL Server.
+    fact_fdic.to_sql(
+        "fact_fdic",
+        connection,
+        index=False,
+        if_exists="replace",
+        dtype={
+            "branch_office_deposits": sa.BigInteger,
+            "total_assets": sa.BigInteger,
+            "total_domestic_deposits": sa.BigInteger,
+            "total_deposits": sa.BigInteger,
+            "branch_key": sa.INTEGER,
+            "bank_key": sa.INTEGER,
+            "location_key": sa.INTEGER,
+            "date_key": sa.INTEGER,
+            "charter_key": sa.INTEGER,
+            "fdic_key": sa.INTEGER,
+            "currency_key": sa.INTEGER,
+            "area_population_key": sa.INTEGER,
+            "deposit_code_key": sa.INTEGER,
+            "assets_key": sa.INTEGER,
+        },
+    )
 
     # Creating the composite primary key for the fact table
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN branch_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN bank_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN location_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN date_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN charter_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN fdic_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN currency_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN area_population_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN deposit_code_key INT NOT NULL""")
-    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN assets_key INT NOT NULL""")
-    connection.execute("""
-    ALTER TABLE fact_fdic ADD PRIMARY KEY (branch_key, bank_key, location_key, date_key, charter_key, fdic_key, currency_key,
-                                            area_population_key, deposit_code_key, assets_key)
-    """)
+    connection.execute(
+        """ ALTER TABLE fact_fdic ALTER COLUMN branch_key INT NOT NULL;"""
+    )
+    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN bank_key INT NOT NULL;""")
+    connection.execute(
+        """ ALTER TABLE fact_fdic ALTER COLUMN location_key INT NOT NULL;"""
+    )
+    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN date_key INT NOT NULL;""")
+    connection.execute(
+        """ ALTER TABLE fact_fdic ALTER COLUMN charter_key INT NOT NULL;"""
+    )
+    connection.execute(""" ALTER TABLE fact_fdic ALTER COLUMN fdic_key INT NOT NULL;""")
+    connection.execute(
+        """ ALTER TABLE fact_fdic ALTER COLUMN currency_key INT NOT NULL;"""
+    )
+    connection.execute(
+        """ ALTER TABLE fact_fdic ALTER COLUMN area_population_key INT NOT NULL;"""
+    )
+    connection.execute(
+        """ ALTER TABLE fact_fdic ALTER COLUMN deposit_code_key INT NOT NULL;"""
+    )
+    connection.execute(
+        """ ALTER TABLE fact_fdic ALTER COLUMN assets_key INT NOT NULL;"""
+    )
+    connection.execute(
+        """
+        ALTER TABLE fact_fdic ADD PRIMARY KEY (branch_key, bank_key, location_key, date_key, charter_key, fdic_key, currency_key,
+                                                area_population_key, deposit_code_key, assets_key);
+        """
+    )
 
     # creating the foreign key constraints
-    connection.execute("""
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (branch_key) REFERENCES dim_branch (branch_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (bank_key) REFERENCES dim_bank (bank_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (location_key) REFERENCES dim_location (location_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (date_key) REFERENCES dim_date (date_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (charter_key) REFERENCES dim_charter (charter_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (fdic_key) REFERENCES dim_fdic (fdic_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (currency_key) REFERENCES dim_currency (currency_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (area_population_key) REFERENCES dim_area_population (area_population_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (deposit_code_key) REFERENCES dim_deposit_code (deposit_code_key);
-    """)
-    connection.execute("""
+    """
+    )
+    connection.execute(
+        """
     ALTER TABLE fact_fdic
         ADD FOREIGN KEY (assets_key) REFERENCES dim_assets (assets_key);
-    """)
-
-
-
-
-
+    """
+    )
--- sql_server_connection.py
+++ sql_server_connection.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import sqlalchemy as sa
 from sqlalchemy import create_engine
-SERVER = 'DESKTOP-PKB226U' # altered for security
-DATABASE = 'bank_data' # altered for security
-DRIVER = 'SQL Server Native Client 11.0'  # altered for security
-DATABASE_CONNECTION = f'mssql://@{SERVER}/{DATABASE}?driver={DRIVER}'
+
+
+# Move these to env file.
+SERVER = "DESKTOP-PKB226U"  # altered for security
+DATABASE = "bank_data"  # altered for security
+DRIVER = "SQL Server Native Client 11.0"  # altered for security
+DATABASE_CONNECTION = f"mssql://@{SERVER}/{DATABASE}?driver={DRIVER}"
 engine = create_engine(DATABASE_CONNECTION)
-#connection = engine.connect()
+# connection = engine.connect()
